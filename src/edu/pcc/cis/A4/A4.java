c8bc946ffb837ff94eef3cc997578f3d67942f6c
Tony Pham <phamt364@gmail.com>
2018-02-01 09:07:08 -0800

    added main method along with a few print statements

--- src/edu/pcc/cis/A4/A4.java
+++ src/edu/pcc/cis/A4/A4.java
@@ -1,4 +1,9 @@
 package edu.pcc.cis.A4;
-
+//Created By Tony Pham
 public class A4 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("First project created in IntelliJ");
+        System.out.println("Created by Tony Pham");
+    }
 }
